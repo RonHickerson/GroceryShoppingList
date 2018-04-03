bff41b80045136ad44702a72379a269ec0ca9c1e
Ron <RonaldJHickerson@yahoo.com>
2018-04-03 13:43:56 -0500

    Created ArrayList for groceries, added groceries to list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.sql.SQLOutput;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -11,9 +12,15 @@ public class Main {
 
         // Create 3 Grocery Items
         GroceryItem eggs = new GroceryItem("Eggs", 3, LocalDateTime.now());
-        GroceryItem milk = new GroceryItem("Milk", 3, LocalDateTime.now());
-        GroceryItem bread = new GroceryItem("Bread", 3, LocalDateTime.now());
+        GroceryItem milk = new GroceryItem("Milk", 4, LocalDateTime.now());
+        GroceryItem bread = new GroceryItem("Bread", 2, LocalDateTime.now());
         System.out.println("\tGrocery List: \n" + eggs + "\n" + milk + "\n" + bread);
+
+        // Store Items in ArrayList
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(eggs);
+        groceryList.add(milk);
+        groceryList.add(bread);
 
 
     }
